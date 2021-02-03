5fe88bde5088c95390e3f1bda2cd39dc92541574
alejandrodavidvela <alex.d.vela@gmail.com>
2021-02-02 19:55:04 -0600

    Course Model and Seeding

--- seeder.js
+++ seeder.js
@@ -6,9 +6,10 @@ const dotenv = require('dotenv')
 // Load env variables
 dotenv.config({path: './config/config.env'})
 
-//Load models
+//Load models (1st Step)
 const Bootcamp = require('./models/Bootcamp')
-const { connect } = require('http2')
+const Course = require('./models/Course')
+
 
 
 //Connect to DB
@@ -19,13 +20,15 @@ mongoose.connect(process.env.MONGO_URI, {
     useUnifiedTopology: true
 })
 
-//Read JSON files 
+//Read JSON files (2nd Step)
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'))
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'))
 
-//Import Bootcamps Data into DB
+//Import Bootcamps Data into DB(3rd Step)
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps)
+        await Course.create(courses)
         console.log('Data Imported...'.green.inverse)
         process.exit()
     } catch (err) {
@@ -33,10 +36,11 @@ const importData = async () => {
     }
 }
 
-//Delete Bootcamps Data from DB
+//Delete Bootcamps Data from DB (4th Step)
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany()
+        await Course.deleteMany()
         console.log('Data Destroyed...'.red.inverse)
         process.exit()
     } catch (err) {
