e3e56177a97258a7ea6b693c0cc7313db148a5dc
alejandrodavidvela <alex.d.vela@gmail.com>
2021-02-03 23:26:27 -0600

    Sending JWT in a Cookie

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const colors = require('colors')
+const fileupload = require('express-fileupload')
+const cookieParser = require('cookie-parser')
 const errorHandler = require('./middleware/error')
 const connectDB = require('./config/db')
-const fileupload = require('express-fileupload')
 
 // Load env vars
 dotenv.config({ path: './config/config.env'})
@@ -22,6 +23,9 @@ const app = express()
 
 //Body parser
 app.use(express.json())
+
+// Cookie parser
+app.use(cookieParser())
 
 // Dev logging middleware with morgan
 if(process.env.NODE_ENV === 'development'){
