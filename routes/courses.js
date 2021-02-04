9560116ab41d2ac3adbae1e26ada80c10fa2bddf
alejandrodavidvela <alex.d.vela@gmail.com>
2021-02-04 01:16:18 -0600

    Roles Authorization

--- routes/courses.js
+++ routes/courses.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { protect } = require('../middleware/auth')
+const { protect, authorize } = require('../middleware/auth')
 
 const { 
     getCourses,
@@ -20,11 +20,11 @@ router
         path: 'bootcamp',
         select: 'name description'
     }), getCourses)
-    .post(protect, addCourse)
+    .post(protect, authorize('publisher', 'admin'), addCourse)
 router
     .route('/:id')
     .get(getCourse)
-    .put(protect, updateCourse)
-    .delete(protect, deleteCourse)
+    .put(protect, authorize('publisher', 'admin'), updateCourse)
+    .delete(protect, authorize('publisher', 'admin'), deleteCourse)
 
 module.exports = router
