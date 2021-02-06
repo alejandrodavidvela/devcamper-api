111f66afb40450a063baf57de734a5c7aaf48a4c
alejandrodavidvela <alex.d.vela@gmail.com>
2021-02-06 14:48:36 -0600

    Forgot Password - Send Email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
+const sendEmail = require('../utils/sendEmail')
 const User = require('../models/User')
 
 
@@ -79,10 +80,30 @@ exports.forgotPassword = asyncHandler(async (req,res,next) => {
     //Save the user
     await user.save({ validateBeforeSave: false})
 
-    res.status(200).json({
-        success: true,
-        data: user
-    })
+    // Create reset url
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}`
+
+    const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: 'Password reset token',
+            message,
+        })
+
+        res.status(200).json({ success: true, data: 'Email sent' })
+    } catch (err) {
+        console.log(err)
+        user.getResetPasswordToken = undefined
+        user.resetPasswordExpire = undefined
+
+        await user.save({ validateBeforeSave: false})
+
+        return next(new ErrorResponse('Email could not be sent', 500))
+
+    }
+
 })
 
 // Get token from model, create cookie and send response
@@ -91,7 +112,9 @@ const sendTokenResponse = (user, statusCode, res) => {
     const token = user.getSignedJwtToken()
 
     const options = {
-        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000,
+        ),
         httpOnly: true
     }
 
